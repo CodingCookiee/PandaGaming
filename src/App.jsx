c78c065a062a3b6abc0d25d545a7b5eb335749bd
CodingCookiee <rzqutubawan@gmail.com>
2024-12-02 14:27:48 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">App</div>;
+const App = () => {
+  return (
+    <div className='App'>
+      App
+    </div>
+  )
 }
 
-export default App;
+export default App
